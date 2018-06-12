fb19787580a25700d1ae15c22487484292050579
aKochHM <akoch@hm.edu>
2018-06-12 21:24:57 +0200

    lol

---
+++ app/src/main/java/com/hm_master/masterapp/Fragment_Bib.java
@@ -0,0 +1,20 @@
+package com.hm_master.masterapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_Bib extends Fragment {
+    @Nullable
+
+    View myView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        myView = inflater.inflate(R.layout.layout_bib,container,false);
+        return myView;
+    }
+}
